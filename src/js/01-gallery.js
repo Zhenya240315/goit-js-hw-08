c8cc9302d033e8b4fc922d56082788c8a4c4bd4a
Zhenya240315 <105649502+Zhenya240315@users.noreply.github.com>
2022-12-01 16:18:17 +0200

    hw-8-finish

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+// HW-7
+const galleryContainer = document.querySelector('.gallery');
+
+function createGallaryMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<li>
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>`
+    )
+    .join('');
+}
+
+const galleryImg = createGallaryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryImg);
+galleryContainer.addEventListener('click', onImgClick);
+
+function onImgClick(evt) {
+  blockStandartAction(evt);
+}
+// блокирует стандартные функц.браузера(не открывает картинку по ссылке)
+function blockStandartAction(evt) {
+  evt.preventDefault();
+}
+
+const galleryEl = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionsDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,58 @@
+import throttle from 'lodash.throttle';
+import { setStorage, getStorage, removeStorage } from './stor';
 
+const form = document.querySelector('.feedback-form');
+const storageKey = 'feedback-form-state';
+let storage = getStorage(storageKey);
+let formData = {};
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(event) {
+  // отримуємо та записуємо данні в сховище
+  formData = storage;
+  formData[event.target.name] = event.target.value;
+
+  setStorage(storageKey, formData);
+}
+
+function onFormSubmit(event) {
+  //кнопка submit. перевіряємо поля, вивонимо в констоль,  очищуемо форму
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  if (email.value === '' || message.value === '') {
+    return alert('Будь ласка заповніть всі поля!');
+  }
+
+  console.log({ Email: email.value, Message: message.value });
+
+  formData = { email: '', message: '' };
+  storage = formData;
+  setStorage(storageKey, formData);
+  event.currentTarget.reset();
+}
+
+function containsАormValues() {
+  // заповнюєо форму після завантаження
+  if (!storage) {
+    storage = {};
+    return;
+  }
+
+  if (storage.email === undefined) {
+    form.elements.email.value = '';
+  } else {
+    form.elements.email.value = storage.email;
+  }
+
+  if (storage.message === undefined) {
+    form.elements.message.value = '';
+  } else {
+    form.elements.message.value = storage.message;
+  }
+}
+containsАormValues();
